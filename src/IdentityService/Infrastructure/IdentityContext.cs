f744bac8aecf59c8b2f3fa61b539451213693eae
Skowoo <skowoo@tlen.pl>
2024-07-30 13:09:09 +0200

    Project structure refactorized and IdentityService context bugfix

--- src/IdentityService/Infrastructure/IdentityContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace IdentityServiceAPI.Infrastructure
-{
-    public class IdentityContext(DbContextOptions<IdentityContext> options) : IdentityDbContext(options)
-    {
-    }
-}
---
+++ src/Services/Identity/IdentityService/Infrastructure/IdentityContext.cs
@@ -0,0 +1,10 @@
+﻿using IdentityServiceAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityServiceAPI.Infrastructure
+{
+    public class IdentityContext(DbContextOptions<IdentityContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+    }
+}
