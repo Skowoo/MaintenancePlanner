972a7d4392f774d9b4547e97f5290f717c30782f
Skowoo <skowoo@tlen.pl>
2024-09-13 11:52:24 +0200

    Testing base creation in progress

---
+++ tests/Services/Action/ActionService.FunctionalTests/Setup/ActionServiceFactory.cs
@@ -0,0 +1,28 @@
+﻿using ActionServiceAPI.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace ActionService.FunctionalTests.Setup
+{
+    public class ActionServiceFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services => {
+                var originalContext = services.SingleOrDefault(x => x.ServiceType == typeof(DbContextOptions<ActionContext>));
+                services.Remove(originalContext!);
+
+                var dbConnection = services.SingleOrDefault(d => d.ServiceType == typeof(DbConnection));
+                services.Remove(dbConnection!);
+
+                services.AddEntityFrameworkInMemoryDatabase()
+                    .AddDbContext<ActionContext>((container, options) =>
+                    {
+                        options.UseInMemoryDatabase("TestDatabase")
+                        .UseInternalServiceProvider(container);
+                    });
+            });
+        }
+    }
+}
---
+++ tests/Services/Action/ActionService.FunctionalTests/Setup/TestScenarioBase.cs
@@ -0,0 +1,35 @@
+﻿using ActionServiceAPI.Domain.Models;
+using ActionServiceAPI.Infrastructure.Data;
+using ActionServiceAPI.Web;
+
+namespace ActionService.FunctionalTests.Setup
+{
+    public class TestScenarioBase
+    {
+        protected static HttpClient GetClient()
+        {
+            var factory = new ActionServiceFactory<Program>();
+            var scope = factory.Services.CreateScope();
+            SeedDatabase(scope);
+            return factory.CreateClient();
+        }
+
+        static void SeedDatabase(IServiceScope scope)
+        {
+            var context = scope.ServiceProvider.GetRequiredService<ActionContext>();
+            context.Database.EnsureCreated();
+
+            context.AvailableParts.AddRange(
+                new AvailablePart(1, 10)
+            );
+
+            context.SaveChanges();
+
+            context.Employees.AddRange(
+                new Employee("TestEmployee")
+            );
+
+            context.SaveChanges();
+        }
+    }
+}
