5f6b7b6bbab87359d4074dcdb08b25d9421eb686
Skowoo <skowoo@tlen.pl>
2024-09-19 09:16:26 +0200

    IdentityService functional tests framework created

---
+++ tests/Services/Identity/IdentityService.FunctionalTests/Setup/TestScenarioBase.cs
@@ -0,0 +1,49 @@
+﻿using IdentityServiceAPI.Infrastructure;
+using IdentityServiceAPI.Models;
+using IdentityServiceAPI.Services;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityService.FunctionalTests.Setup
+{
+    public class TestScenarioBase
+    {
+        public const string IdentityServiceUri = "https://localhost:7001/api/v1/Identity/";
+        public const string AdminLogin = "Admin";
+        public const string AdminEmail = "admin@adm.eu";
+        public const string AdminPassword = "Adm1n3k!!!";
+        public const string AdminRoleName = "Admin";
+
+
+        protected static HttpClient GetClient()
+        {
+            var factory = new IdentityServiceFactory();
+            var scope = factory.Services.CreateScope();
+            SeedDatabase(scope);
+            return factory.CreateClient();
+        }
+
+        static void SeedDatabase(IServiceScope scope)
+        {
+            var context = scope.ServiceProvider.GetRequiredService<IdentityContext>();
+            context.Database.EnsureCreated();
+
+            var identityService = scope.ServiceProvider.GetRequiredService<IIdentityService>();
+
+            var adminRegisterModel = new RegisterModel()
+            {
+                Login = AdminLogin,
+                Email = AdminEmail,
+                Password = AdminPassword
+            };
+            identityService.RegisterNewUser(adminRegisterModel, AdminPassword).Wait();
+
+            var role = new IdentityRole(AdminRoleName);
+            context.Roles.Add(role);
+            context.SaveChanges();
+
+            //var admin = context.Users.Single(u => u.UserName == AdminLogin);
+            //identityService.AddUserToRole(admin, AdminRoleName).Wait();
+            //context.SaveChanges();
+        }
+    }
+}
---
+++ tests/Services/Identity/IdentityService.FunctionalTests/UserFunctionalities_OkTests.cs
@@ -0,0 +1,32 @@
+﻿using IdentityService.FunctionalTests.Setup;
+using IdentityServiceAPI.Models;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace IdentityService.FunctionalTests
+{
+    [TestClass]
+    public class UserFunctionalities_OkTests : TestScenarioBase
+    {
+        [TestMethod]
+        public void GetAllUsers_ReturnsUsersList()
+        {
+            using var client = GetClient();
+            HttpRequestMessage request = new()
+            {
+                RequestUri = new Uri(IdentityServiceUri + $"GetAllUsers"),
+                Method = HttpMethod.Get
+            };
+
+            var response = client.SendAsync(request).Result;
+
+            var responseContent = response.Content.ReadAsStringAsync().Result;
+            var users = JsonConvert.DeserializeObject<List<ApplicationUserExternalModel>>(responseContent);
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            Assert.IsNotNull(users);
+            Assert.AreEqual(1, users.Count);
+            Assert.AreEqual(AdminLogin, users[0].UserName);
+        }
+    }
+}
